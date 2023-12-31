33e816d5c4b5c65b6b3191e610952e4be3c30baa
Bibest <bibhestee@gmail.com>
2023-12-31 16:02:31 +0100

    rectify lazy loading requirement(unique method)

--- api/models/db.py
+++ api/models/db.py
@@ -2,8 +2,9 @@
 """
 Database Controller Module
 """
+from flask import abort
 from api.models import db_engine
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from sqlalchemy.exc import IntegrityError
 
 db = db_engine
@@ -37,27 +38,36 @@ class Database():
         """ get a single model """
         from api import app
         with app.app_context():
-            return db.one_or_404(db.select(model).filter_by(id=id))
+            try:
+                # Alternate statement of one_or_404
+                statement = db.select(model).filter_by(id=id)
+                # unique is used for joined lazy loading (one to many relationship)
+                return db.session.execute(statement).unique().scalar_one()
+            except (NoResultFound, MultipleResultsFound):
+                abort(404)
 
     def get_all(self, model):
         """ get all model """
         from api import app
         with app.app_context():
-            objs = db.session.execute(db.select(model)).all()
+            statement = db.select(model)
+            objs = db.session.execute(statement).unique().all()
             return [obj[0].to_json() for obj in objs]
 
     def get_my_exams(self, model, admin_id):
         """ get all my exam model """
         from api import app
         with app.app_context():
-            objs = db.session.execute(db.select(model).filter_by(admin_id=admin_id)).all()
+            statement = db.select(model).filter_by(admin_id=admin_id)
+            objs = db.session.execute(statement).unique().all()
             return [obj[0].to_json() for obj in objs]
         
     def get_exam_results(self, model, exam_id: str):
         """ get all results related to exam_id """
         from api import app
         with app.app_context():
-            objs = db.session.execute(db.select(model).filter_by(exam_id=exam_id)).all()
+            statement = db.select(model).filter_by(exam_id=exam_id)
+            objs = db.session.execute(statement).unique().all()
             return [obj[0].to_json() for obj in objs]
         
     def update(self, model, id: str,  **kwargs) -> None:
